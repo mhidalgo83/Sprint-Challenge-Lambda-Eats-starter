8dc3f1f7a31d80d84b79df38418a0e2f37eb322f
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-05-02 11:44:32 -0700

    dropdown working

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ import Dropdown from "./Dropdown";
 import Header from "./Header";
 import Input from "./Input";
 import styled from "styled-components";
+import axios from "axios";
 
 const PizzaHeader = styled(Header)`
   margin: 2% auto;
@@ -15,14 +16,36 @@ const FormDiv = styled.div`
 `;
 
 const Form = () => {
+  const [post, setPost] = useState([]);
   const [formState, setFormState] = useState({
     name: "",
     size: "",
     toppings: [],
+    sauce: "",
     instructions: "",
   });
 
   useEffect(() => {}, [formState]);
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then((res) => {
+        console.log(res.data);
+        setPost(res.data);
+        setFormState({
+          name: "",
+          size: "",
+          toppings: [],
+          sauce: "",
+          instructions: "",
+        });
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
 
   const inputChange = (e) => {
     console.log("input changed!", e.target.value);
@@ -30,16 +53,14 @@ const Form = () => {
     const formData = {
       ...formState,
       [e.target.name]:
-        e.target.type === "checkbox" || "radio"
-          ? e.target.checked
-          : e.target.value,
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
     };
     setFormState(formData);
   };
   return (
     <FormDiv>
       <PizzaHeader>Build Your Own Pizza</PizzaHeader>
-      <form>
+      <form onSubmit={formSubmit}>
         <h2>Build Your Own Pizza</h2>
         <div>
           <h2>Who is this for?</h2>
@@ -58,22 +79,47 @@ const Form = () => {
           <h2>Choice of Size</h2>
           <p>Required</p>
         </div>
-        <Dropdown />
+        <select
+          id="size"
+          name="size"
+          onChange={inputChange}
+          value={formState.size}
+        >
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+          <option value="Extra Large">Extra Large</option>
+        </select>
         <div>
           <h2>Choice of Sauce</h2>
           <p>Required</p>
         </div>
         <div>
-          <CheckBox type="radio" value="original" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="radio"
+            value="original"
+            onChange={inputChange}
+          >
             Original Red
           </CheckBox>
-          <CheckBox type="radio" value="garlic" onChange={inputChange}>
+          <CheckBox
+            id="garlic"
+            type="radio"
+            value="garlic"
+            onChange={inputChange}
+          >
             Garlic Ranch
           </CheckBox>
-          <CheckBox type="radio" value="bbq" onChange={inputChange}>
+          <CheckBox id="bbq" type="radio" value="bbq" onChange={inputChange}>
             BBQ Sauce
           </CheckBox>
-          <CheckBox type="radio" value="alfredo" onChange={inputChange}>
+          <CheckBox
+            id="alfredo"
+            type="radio"
+            value="alfredo"
+            onChange={inputChange}
+          >
             Alfredo
           </CheckBox>
         </div>
@@ -82,13 +128,24 @@ const Form = () => {
           <p>Choose up to 10</p>
         </div>
         <div>
-          <CheckBox type="checkbox" value="Pepperoni" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Pepperoni"
+            onChange={inputChange}
+          >
             Pepperoni
           </CheckBox>
-          <CheckBox type="checkbox" value="Sausage" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Sausage"
+            onChange={inputChange}
+          >
             Sausage
           </CheckBox>
           <CheckBox
+            id="original"
             type="checkbox"
             value="Canadian Bacon"
             onChange={inputChange}
@@ -96,38 +153,57 @@ const Form = () => {
             Canadian Bacon
           </CheckBox>
           <CheckBox
+            id="original"
             type="checkbox"
             value="Spice Italian Sausage"
             onChange={inputChange}
           >
-            "Spice Italian Sausage"
+            Spice Italian Sausage
           </CheckBox>
           <CheckBox
+            id="original"
             type="checkbox"
             value="Grilled Chicken"
             onChange={inputChange}
           >
             Grilled Chicken
           </CheckBox>
-          <CheckBox type="checkbox" value="Onions" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Onions"
+            onChange={inputChange}
+          >
             Onions
           </CheckBox>
-          <CheckBox type="checkbox" value="Green Pepper" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Green Pepper"
+            onChange={inputChange}
+          >
             Green Pepper
           </CheckBox>
         </div>
         <div>
           <CheckBox
+            id="original"
             type="checkbox"
             value="Diced Tomatoes"
             onChange={inputChange}
           >
             Diced Tomatoes
           </CheckBox>
-          <CheckBox type="checkbox" value="Black Olives" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Black Olives"
+            onChange={inputChange}
+          >
             Black Olives
           </CheckBox>
           <CheckBox
+            id="original"
             type="checkbox"
             value="Roasted Garlic"
             onChange={inputChange}
@@ -135,19 +211,35 @@ const Form = () => {
             Roasted Garlic
           </CheckBox>
           <CheckBox
+            id="original"
             type="checkbox"
             value="Artichoke Hearts"
             onChange={inputChange}
           >
             Artichoke Hearts
           </CheckBox>
-          <CheckBox type="checkbox" value="Three Cheese" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Three Cheese"
+            onChange={inputChange}
+          >
             Three Cheese
           </CheckBox>
-          <CheckBox type="checkbox" value="Pineapple" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Pineapple"
+            onChange={inputChange}
+          >
             Pineapple
           </CheckBox>
-          <CheckBox type="checkbox" value="Extra Cheese" onChange={inputChange}>
+          <CheckBox
+            id="original"
+            type="checkbox"
+            value="Extra Cheese"
+            onChange={inputChange}
+          >
             Extra Cheese
           </CheckBox>
         </div>
@@ -156,7 +248,7 @@ const Form = () => {
           <p>Choose up to 1</p>
         </div>
         <div>
-          <CheckBox type="checkbox" value="Gluten Free Crust">
+          <CheckBox id="original" type="checkbox" value="Gluten Free Crust">
             Gluten Free Crust
           </CheckBox>
         </div>
@@ -164,7 +256,12 @@ const Form = () => {
           <h2>Special Instructions</h2>
         </div>
         <div>
-          <Input type="text" />
+          <textarea
+            type="text"
+            id="instructions"
+            name="instructions"
+            onChange={inputChange}
+          ></textarea>
         </div>
         <Button>Submit</Button>
       </form>
